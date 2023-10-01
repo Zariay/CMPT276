ba6c29a2d4f0d221b40e7f97589f2d4b94edb08f
cyl74 <edwardlai1008@gmail.com>
2023-10-01 03:41:26 -0700

    Switch template to MVC, but CSS not working for me when I put it in to this repository

---
+++ MyWebApp/Models/Characters.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWebApp.Models
+{
+    public class Characters
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
