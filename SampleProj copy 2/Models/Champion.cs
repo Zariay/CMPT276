07ab76e60a401de2d7e0bcf618b856a9e9cec467
Raman Kumar <raman16894@gmail.com>
2023-10-04 12:48:57 -0700

    Champion List

---
+++ SampleProj copy 2/Models/Champion.cs
@@ -0,0 +1,20 @@
+
+// Champion.cs file. Located in the Models folder.
+using System;
+
+namespace SampleProj.Models
+{
+    public class Champion
+    {
+        public Champion()
+        {
+            Name = ""; // Set a default value for Name
+            Category = ""; // Set a default value for Attribute
+            ImageUrl = ""; // Set a default value for ImageUrl
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ SampleProj copy 2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleProj.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
