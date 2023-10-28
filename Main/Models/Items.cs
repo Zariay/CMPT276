390ca12366b6eb75d66c09995537562410b3f355
liam <lma90@sfu.ca>
2023-10-27 20:47:04 -0700

    Item summation for bonus stats is done. All stats for the Item, ChampionStat and Champion datatypes should be of same and correct data type.

--- Main/Models/Items.cs
+++ Main/Models/Items.cs
@@ -7,17 +7,17 @@ namespace SampleProj.Models
         public class Item
     {
         public string? Name { get; set; }
-        public int HealthBonus { get; set; }                //HP
+        public double HealthBonus { get; set; }                //HP
         public double HealthRegenerationBonus { get; set; }     // HP5
         public double bonus_heal_shield_power { get; set; }      // for support items
-        public int ManaBonus { get; set; }                      // MP
+        public double ManaBonus { get; set; }                      // MP
         public double ManaRegenerationBonus { get; set; }       // MP5
-        public int AttackDamageBonus { get; set; }              // AD
+        public double AttackDamageBonus { get; set; }              // AD
         public double AttackSpeedBonus { get; set; }            // AS (attacks per second)
         public int AttackRangeBonus { get; set; }               // rang
-        public int ArmorBonus { get; set; }                     // AR
-        public int MagicResistanceBonus { get; set; }           // MR
-        public int MovementSpeedBonus { get; set; }             // MS
+        public double ArmorBonus { get; set; }                     // AR
+        public double MagicResistanceBonus { get; set; }           // MR
+        public double MovementSpeedBonus { get; set; }             // MS
         public double CriticalStrikeChanceBonus { get; set; }   // Crit
         public double CriticalStrikeDamageBonus { get; set; }  // CritDmg
         public double LifeStealBonus { get; set; }              // LS
@@ -28,40 +28,42 @@ namespace SampleProj.Models
         public double bonus_ap { get; set; }                    // ability power
         public double bonus_ap_amp { get; set; }                // AP amp, so, deathcap
         public double bonus_MS_amp { get; set; }                // amp to movement speed
-        public double bonus_lethality { get; set; }
-        public double bonus_magic_pen { get; set; }
+        public int bonus_lethality { get; set; }
+        public int bonus_magic_pen { get; set; }
         public double bonus_percent_MR_pen { get; set; }
         public double bonus_percent_AR_pen { get; set; }
         public double bonus_ability_haste { get; set; }
+        public int bonus_gold_generation {  get; set; }
 
     public Item(){
             Name="";
-            HealthBonus=0;
-            HealthRegenerationBonus=0;
-            bonus_heal_shield_power = 0;
-            ManaBonus =0;
-            ManaRegenerationBonus=0;
-            AttackDamageBonus=0;
-            AttackSpeedBonus=0;
+            HealthBonus=0d;
+            HealthRegenerationBonus=0d;
+            bonus_heal_shield_power = 0d;
+            ManaBonus =0d;
+            ManaRegenerationBonus=0d;
+            AttackDamageBonus=0d;
+            AttackSpeedBonus=0d;
             AttackRangeBonus=0;
-            ArmorBonus=0;
-            MagicResistanceBonus=0;
-            MovementSpeedBonus=0;
-            CriticalStrikeChanceBonus=0;
-            CriticalStrikeDamageBonus=0;
-            LifeStealBonus=0;
-            SpellVampBonus=0;
-            TenacityBonus=0;
-            omnivamp_bonus = 0;
-            slow_res_bonus = 0;
-            bonus_ap = 0;
-            bonus_ap_amp = 0;
-            bonus_MS_amp = 0;
+            ArmorBonus=0d;
+            MagicResistanceBonus=0d;
+            MovementSpeedBonus=0d;
+            CriticalStrikeChanceBonus=0d;
+            CriticalStrikeDamageBonus=0d;
+            LifeStealBonus=0d;
+            SpellVampBonus=0d;
+            TenacityBonus=0d;
+            omnivamp_bonus = 0d;
+            slow_res_bonus = 0d;
+            bonus_ap = 0d;
+            bonus_ap_amp = 0d;
+            bonus_MS_amp = 0d;
             bonus_lethality = 0;
             bonus_magic_pen = 0;
-            bonus_percent_MR_pen = 0;
-            bonus_percent_AR_pen = 0;
-            bonus_ability_haste = 0;
+            bonus_percent_MR_pen = 0d;
+            bonus_percent_AR_pen = 0d;
+            bonus_ability_haste = 0d;
+            bonus_gold_generation = 0;
         }
     }
 
@@ -72,8 +74,8 @@ namespace SampleProj.Models
     // Health,HealthRegeneration,..., in ChampionStat
     public class ChampionStat
     {
-        //Until either the database or API get up and running, this will just be temporary to see how things look
         public Champion? ChampionData { get; set; }                 // The Champion
+        public int Level { get; set; }                        // Current level
         public double Health { get; set; }                    // HP
         public double HealthRegeneration { get; set; }     // HP5
         public double heal_shield_power { get; set; }      // for support items
@@ -96,63 +98,77 @@ namespace SampleProj.Models
         public double energy_regen {  get; set; }
         public double ability_haste {  get; set; }        // CDR/Ability Haste
         public double ability_power { get; set; }         // AP
-        public double magic_pen {  get; set; }            // AP lethality
+        public int magic_pen {  get; set; }            // AP lethality
         public double percent_MR_pen {  get; set; }       // MR pen %
-        public double lethality {  get; set; }            // lethality
+        public int lethality {  get; set; }            // lethality
         public double percent_AR_pen { get; set; }        // armor pen %
         public int gold_generation { get; set; }          // gold gained per second
 
 
         public ChampionStat(){
             ChampionData= new Champion();
-            Health = 550;
-            HealthRegeneration = 8.0;
-            Mana = 350;
-            ManaRegeneration = 6.0;
-            AttackDamage = 60;
-            AttackSpeed = 0.7;
-            AttackRange = 175;
-            Armor = 30;
-            MagicResistance = 30;
-            MovementSpeed = 345;
-            CriticalStrikeChance = 0.25;
-            CriticalStrikeDamage = 200;
+            Level = 1;
+            Health = 0d;
+            heal_shield_power = 0d;
+            HealthRegeneration = 0d;
+            Mana = 0d;
+            ManaRegeneration = 0d;
+            AttackDamage = 0d;
+            AttackSpeed = 0d;
+            AttackRange = 0;
+            Armor = 0d;
+            MagicResistance = 0d;
+            MovementSpeed = 0d;
+            CriticalStrikeChance = 0d;
+            CriticalStrikeDamage = 0d;
             LifeSteal = 0d;
             SpellVamp = 0d;
+            Omnivamp = 0d;
             Tenacity = 0d;
-            energy = 200;
+            slow_resist = 0d;
+            energy = 0d;
+            energy_regen = 0d;
+            ability_haste = 0d;
+            ability_power = 0d;
+            magic_pen = 0;
+            percent_MR_pen = 0d;
+            lethality = 0;
+            percent_AR_pen = 0d;
+            gold_generation = 0;
         }
 
-        public void calculate_stats() 
+        // clear all values but retain champion data
+        public void clear()
         {
-            // Call calculation for HP
-
-            // Call calculation for HP regen
-
-            // Call calculation for mana
-
-            // Call calculation for mana regen
-
-            // Call calculation for energy/resource
-
-            // Call calculation for energy/resource regen
-
-            // Call calculation for AD
-
-            // Call calculation for AS
-
-            // Call calculation for AP
-
-            // Call calculation for Cooldown reduction
-
-            // Call calculation for Armor
-
-            // Call calculation for MR
-
-
-
-
+            Health = 0d;
+            heal_shield_power = 0d;
+            HealthRegeneration = 0d;
+            Mana = 0d;
+            ManaRegeneration = 0d;
+            AttackDamage = 0d;
+            AttackSpeed = 0d;
+            AttackRange = 0;
+            Armor = 0d;
+            MagicResistance = 0d;
+            MovementSpeed = 0d;
+            CriticalStrikeChance = 0d;
+            CriticalStrikeDamage = 0d;
+            LifeSteal = 0d;
+            SpellVamp = 0d;
+            Omnivamp = 0d;
+            Tenacity = 0d;
+            slow_resist = 0d;
+            energy = 0d;
+            energy_regen = 0d;
+            ability_haste = 0d;
+            ability_power = 0d;
+            magic_pen = 0;
+            percent_MR_pen = 0d;
+            lethality = 0;
+            percent_AR_pen = 0d;
+            gold_generation = 0;
         }
+
     }
 
 
